45e1b64cb0657ba8703382bf1af75c45192043b9
unknown <sanik4144@gmail.com>
2024-05-15 20:20:48 +0600

    frontend

--- face-recognize.py
+++ face-recognize.py
@@ -38,7 +38,7 @@ class FaceRecognition(Camera):
         for (x, y, w, h) in faces:
             roi_gray = gray[y:y+h, x:x+w]
             id_, conf = self.recognizer.predict(roi_gray)
-            if conf >= 60 and conf <= 90:
+            if conf >= 50 and conf <= 90:
                 name = self.labels[id_]
                 cv2.putText(frame, name, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
             cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
